fc3630e2d018d22e3087e6ff5fc2953641142cb4
chohyeon <chohyeonK3@gmail.com>
2023-05-15 16:28:22 +0900

    feat : 이름 검색 기능 구현 - 후보자 collapse 수정
    Issues: #25

--- src/main/webapp/resources/js/manager_search.js
+++ src/main/webapp/resources/js/manager_search.js
@@ -7,18 +7,18 @@ function createCandidate(name, id, grade, major, index) {
     console.log(name, id, grade, major, index)
     const divAccordian = document.getElementById('accordion')
     let divCard = document.createElement('div');
-    divCard.innerHTML = '<div class="card-header" id="heading-' + index + '>\n' +
+    divCard.innerHTML = '<div class="card-header" id="heading-' + cnt + '>\n' +
         '                      <h5 class="mb-0">\n' +
-        '                        <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapse' + index + '" aria-expanded="true" aria-controls="collapse' + index + '">\n' +
+        '                        <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapse-' + cnt + '" aria-expanded="true" aria-controls="collapse-' + cnt + '">\n' +
         '                          기호' + cnt + '번 ' + major + ' ' + grade + '학년 ' + name +'\n' +
         '                        </button>\n' +
         '                      </h5>'
     divAccordian.append(divCard)
 
     let divCollapse = document.createElement('div');
-    divCollapse.setAttribute('id', 'collapse' + cnt)
+    divCollapse.setAttribute('id', 'collapse-' + cnt)
     divCollapse.setAttribute('class', 'collapse show')
-    divCollapse.setAttribute('aria-labelledby', 'heading' + cnt)
+    divCollapse.setAttribute('aria-labelledby', 'heading-' + cnt)
     divCollapse.setAttribute('data-parent', '#accordion')
     divCollapse.innerHTML = '<div class="card-body">\n' +
         '                        <div style="display: flex; border: 1px solid black;">\n' +
@@ -50,9 +50,9 @@ function createCandidate(name, id, grade, major, index) {
     student.major = major
     student.candidate_spec = ''
     student.candidatepromise = ''
-    studentArr.push(student)
+    candidates.push(student)
 
-    console.log(studentArr)
+    console.log(candidates)
 
     cnt++ // 기호 번호 카운팅
 }
