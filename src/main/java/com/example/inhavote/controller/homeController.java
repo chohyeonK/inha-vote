88bc1c17f271fd5435109975fc76c1e8097807fa
CHOI-JEONGYUN <chlxx00@naver.com>
2023-05-27 17:29:00 +0900

    fix: 커밋 수정 - homeController 수정

--- src/main/java/com/example/inhavote/controller/homeController.java
+++ src/main/java/com/example/inhavote/controller/homeController.java
@@ -20,22 +20,18 @@ public class homeController {
     {
         return "manager/manager_Login";
     }
-    @GetMapping("/Result")
-    public String Result() { return "manager/manager_Result"; }
-//    @GetMapping("/UserVote")
-//    public String Vote() { return "user/user_vote"; }
+
+   /* @GetMapping("/UserVote")
+    public String Vote() { return "user/user_vote"; }
     @GetMapping("/UserResult")
     public String userResult() { return "user/user_result"; }
     @GetMapping("/UserHome")
     public String UserHome() { return "user/user_home"; }
     @GetMapping("/UserInfo")
     public String UserInfo() { return "user/user_info"; }
-//    @GetMapping("/UserEmail")
-//    public String UserEmail() { return "user/user_email"; }
-    @GetMapping("/Register")
-    public String Register() { return "manager/manager_Register"; }
-    @GetMapping("/URL")
-    public String URL() { return "manager/manager_URL"; }
+    @GetMapping("/UserEmail")
+    public String UserEmail() { return "user/user_email"; }*/
+
     @GetMapping("/Admin")
     public String Admin() { return "Admin"; }
 
